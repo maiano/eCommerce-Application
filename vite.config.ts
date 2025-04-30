454ec7a345657d62589d8b6d89bc6eecf8049bf5
maiano <maiano@yandex.ru>
2025-04-30 22:24:29 +0200

    chore: install and basic config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -9,5 +10,40 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    coverage: {
+      all: false,
+      exclude: [
+        'src/test/**',
+        '**/types/**',
+        '**/*.d.ts',
+        'src/**/index.ts',
+        'src/main.tsx',
+      ],
+      extension: ['.ts', '.tsx'],
+      include: ['src/**/*'],
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        lines: 80,
+        functions: 70,
+        branches: 60,
+      },
+    },
+    typecheck: {
+      enabled: true,
+      include: ['**/*.test-d.ts'],
+    },
+    css: {
+      modules: { classNameStrategy: 'non-scoped' },
+    },
+    exclude: ['**/node_modules/**', '**/e2e/**'],
+    include: ['**/*.{test,spec}.{ts,tsx}'],
+    environment: 'jsdom',
+    globals: true,
+    maxConcurrency: 4,
+    setupFiles: ['./src/test/setupTests.ts'],
+    cache: { dir: './.vitest-cache' },
   },
 });
