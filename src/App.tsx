5bc4018c64cf0d8f829c203f1432da1dce591d7a
maiano <maiano@yandex.ru>
2025-05-02 21:57:27 +0200

    chore: change config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { HomePage } from "./pages/Home";
+import { HomePage } from '@/pages/Home';
 
 function App() {
-  return <HomePage />
+  return <HomePage />;
 }
 
-export default App
+export default App;
