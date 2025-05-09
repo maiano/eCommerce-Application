721bf1ae5c5bd83916e9081a74a371d76e431145
maiano <maiano@yandex.ru>
2025-05-09 23:13:00 +0200

    feat: add empty login and registration pages

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from 'react-router';
 import { HomePage } from '@/pages/HomePage';
+import { LoginPage } from '@/pages/LoginPage';
 import { NotFoundPage } from '@/pages/NotFoundPage';
+import { RegistrationPage } from '@/pages/RegistrationPage';
+
+const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  REGISTRATION: '/registration',
+} as const;
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path={ROUTES.HOME} element={<HomePage />} />
+      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      <Route path={ROUTES.REGISTRATION} element={<RegistrationPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
