93f7e6a7b29e5d1ce8b8a9606584272f2f5c6ca8
maiano <maiano@yandex.ru>
2025-05-16 15:21:06 +0200

    chore: adjust vitest config

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,11 +1,10 @@
 import { MantineProvider } from '@mantine/core';
 import { render as testingLibraryRender } from '@testing-library/react';
-import type { ReactNode } from 'react';
 import { theme } from '@/app/theme';
 
-export function customRender(ui: ReactNode) {
-  return testingLibraryRender(ui, {
-    wrapper: ({ children }: { children: ReactNode }) => (
+export function customRender(ui: React.ReactNode) {
+  return testingLibraryRender(<>{ui}</>, {
+    wrapper: ({ children }: { children: React.ReactNode }) => (
       <MantineProvider theme={theme}>{children}</MantineProvider>
     ),
   });
--- vitest.setup.mjs
+++ vitest.setup.mjs
@@ -1,9 +1,13 @@
 import '@testing-library/jest-dom/vitest';
 import { vi } from 'vitest';
 
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
+window.HTMLElement.prototype.scrollIntoView = () => {};
+
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: (query: string) => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -12,5 +16,13 @@ Object.defineProperty(window, 'matchMedia', {
     addEventListener: vi.fn(),
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
-  }),
+  })),
 });
+
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+window.ResizeObserver = ResizeObserver;
