d5b5013a801f49607ff0b09ff81545b44cfbd71a
maiano <maiano@yandex.ru>
2025-05-16 14:17:26 +0200

    feat: add connection error output for user

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { AppProvider } from '@/app/providers';
+import '@mantine/core/styles.css';
+import './index.css';
 
 function App() {
   return <AppProvider />;
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,22 +1,20 @@
-import { MantineProvider } from '@mantine/core';
-import { notifications } from '@mantine/notifications';
-import { useEffect } from 'react';
+import { MantineProvider, Notification } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/app/router';
 import { theme } from '@/app/theme';
 import { initApiClient } from '@/shared/lib/commercetools';
 
 export const AppProvider = () => {
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
     const initializeApp = async () => {
       try {
         await initApiClient();
-      } catch (error) {
-        notifications.show({
-          message: 'Failed to connect to commercetools',
-          color: 'red',
-        });
-        console.log('api client init error:', error);
+      } catch (err) {
+        console.error('API client init error:', err);
+        setError('Failed to connect to commercetools');
       }
     };
 
@@ -24,6 +22,24 @@ export const AppProvider = () => {
   }, []);
   return (
     <MantineProvider theme={theme}>
+      {error && (
+        <Notification
+          color="red"
+          withCloseButton
+          closeButtonProps={{ 'aria-label': 'Hide notification' }}
+          onClose={() => setError(null)}
+          style={{
+            maxWidth: '280px',
+            position: 'fixed',
+            top: 20,
+            right: 20,
+            zIndex: 1000,
+            borderRadius: '8px',
+          }}
+        >
+          {error}
+        </Notification>
+      )}
       <RouterProvider router={router} />
     </MantineProvider>
   );
