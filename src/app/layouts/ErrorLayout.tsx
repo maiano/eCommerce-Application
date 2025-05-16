25e15cb5382ac7d64656cabe6bfb8d3acbd1cf9d
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2025-05-16 21:03:52 +0300

    feat: add header

--- src/app/layouts/ErrorLayout.tsx
+++ src/app/layouts/ErrorLayout.tsx
@@ -1,9 +1,14 @@
 import { AppShell } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
+import { Header } from '@/components/Header/Header';
 
 export function ErrorLayout() {
   return (
     <AppShell header={{ height: 60 }} withBorder={false}>
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
+
       <AppShell.Main>
         <Outlet />
       </AppShell.Main>
