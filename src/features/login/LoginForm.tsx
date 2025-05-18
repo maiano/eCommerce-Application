1527f6ac1af2d1812fddee8d2e9a26dc038a9c12
maiano <maiano@yandex.ru>
2025-05-19 01:14:04 +0200

    feat: add zod validation to form

--- src/features/login/LoginForm.tsx
+++ src/features/login/LoginForm.tsx
@@ -1,30 +1,41 @@
 import { Button, Group, PasswordInput, Stack, TextInput } from '@mantine/core';
+import { useForm, zodResolver } from '@mantine/form';
+import { loginSchema } from '@/shared/validation';
+import { LoginFormData } from '@/shared/validation/login-validation';
 
 export function LoginForm() {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate: zodResolver(loginSchema),
+    validateInputOnBlur: true,
+  });
+
+  const handleSubmit = (values: LoginFormData) => {
+    console.log('form submitted:', values);
+  };
+
   return (
-    <form>
+    <form onSubmit={form.onSubmit(handleSubmit)}>
       <Stack>
         <TextInput
           label="Email"
           placeholder="Email"
-          classNames={{ input: 'form-input' }}
           withAsterisk
+          {...form.getInputProps('email')}
         />
 
         <PasswordInput
           label="Password"
           placeholder="Password"
-          classNames={{ input: 'form-input' }}
           withAsterisk
+          {...form.getInputProps('password')}
         />
 
         <Group mt="md">
-          <Button
-            w="100%"
-            type="submit"
-            variant="filled"
-            // style={{ margin: '0.5rem' }}
-          >
+          <Button w="100%" type="submit" variant="filled">
             Sign In
           </Button>
         </Group>
