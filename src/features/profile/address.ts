91c097097d8829280c4da68f29b5837887f08d02
ann-sm <annsmirnovva@gmail.com>
2025-05-30 16:38:54 +0300

    refactor: move setAddressActions function into separate utils module

--- src/features/profile/address.ts
+++ src/features/profile/address.ts
@@ -1,81 +1,8 @@
-import { ClientResponse, Customer, MyCustomerAddAddressAction, MyCustomerAddBillingAddressIdAction, MyCustomerAddShippingAddressIdAction, MyCustomerChangeAddressAction, MyCustomerRemoveBillingAddressIdAction, MyCustomerRemoveShippingAddressIdAction, MyCustomerSetDefaultBillingAddressAction, MyCustomerSetDefaultShippingAddressAction, MyCustomerUpdateAction } from "@commercetools/platform-sdk";
+import { ClientResponse, Customer, MyCustomerAddAddressAction, MyCustomerChangeAddressAction } from "@commercetools/platform-sdk";
 import { getUserInfo } from "./profile";
 import { apiClientManager } from "@/shared/lib/commercetools"
 import { notifyError, notifySuccess } from "@/shared/utils/custom-notifications";
-
-const setAddressActions = (id: string | undefined, currentUser: ClientResponse<Customer> | undefined, isDelivery: boolean, isBilling: boolean, isDefaultDelivery: boolean, isDefaultBilling: boolean) => {
-  let actions: MyCustomerUpdateAction[] = [];
-
-  const setDefaultShippingAddress: MyCustomerSetDefaultShippingAddressAction = {
-    action: 'setDefaultShippingAddress',
-    addressId: id,
-  }
-  
-  const setDefaultBillingAddress: MyCustomerSetDefaultBillingAddressAction = {
-    action: 'setDefaultBillingAddress',
-    addressId: id,
-  }
-
-  const removeDefaultShippingAddress: MyCustomerSetDefaultShippingAddressAction = {
-    action: 'setDefaultShippingAddress',
-    addressId: undefined,
-  }
-  
-  const removeDefaultBillingAddress: MyCustomerSetDefaultBillingAddressAction = {
-    action: 'setDefaultBillingAddress',
-    addressId: undefined,
-  }
-
-  const addShippingAddressId: MyCustomerAddShippingAddressIdAction = {
-    action: 'addShippingAddressId',
-    addressId: id,
-  }
-  
-  const addBillingAddressId: MyCustomerAddBillingAddressIdAction = {
-    action: 'addBillingAddressId',
-    addressId: id,
-  }
-
-  const removeShippingAddressId: MyCustomerRemoveShippingAddressIdAction = {
-    action: 'removeShippingAddressId',
-    addressId: id,
-  }
-
-  const removeBillingAddressId: MyCustomerRemoveBillingAddressIdAction = {
-    action: 'removeBillingAddressId',
-    addressId: id,
-  }
-  
-
-  if (id !== currentUser?.body.defaultShippingAddressId && isDefaultDelivery) {
-    actions.push(setDefaultShippingAddress);
-  }
-  if (id === currentUser?.body.defaultShippingAddressId && !isDefaultDelivery) {
-    actions.push(removeDefaultShippingAddress);
-  }
-  if (!currentUser?.body.shippingAddressIds?.includes(id ?? '') && isDelivery && !actions.includes(setDefaultShippingAddress)) {
-    actions.push(addShippingAddressId);
-  }
-  if (currentUser?.body.shippingAddressIds?.includes(id ?? '') && !isDelivery) {
-    actions.push(removeShippingAddressId);
-  }
-
-
-  if (id !== currentUser?.body.defaultBillingAddressId && isDefaultBilling) {
-    actions.push(setDefaultBillingAddress);
-  } 
-  if (id === currentUser?.body.defaultBillingAddressId && !isDefaultBilling) {
-    actions.push(removeDefaultBillingAddress);
-  }
-  if (!currentUser?.body.billingAddressIds?.includes(id ?? '') && isBilling && !actions.includes(setDefaultBillingAddress)) {
-    actions.push(addBillingAddressId);
-  }
-  if (currentUser?.body.billingAddressIds?.includes(id ?? '') && !isBilling) {
-    actions.push(removeBillingAddressId);
-  }
-  return actions;
-}
-
+import { setAddressActions } from "@/shared/utils/set-address-actions";
 
 export async function deleteAddress(id: string | undefined) {
   const client = apiClientManager.get();
