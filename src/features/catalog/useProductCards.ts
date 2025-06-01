0e94e40c2da0bc581e5df4a533dde63e544a4f5f
maiano <maiano@yandex.ru>
2025-06-01 13:12:15 +0200

    refactor: useProductCards hook

--- src/features/catalog/useProductCards.ts
+++ src/features/catalog/useProductCards.ts
@@ -1,59 +1,7 @@
-import { ProductProjection } from '@commercetools/platform-sdk';
+import { mapProductToCard } from '@/features/catalog/map-product-to-card-util';
+import { sortMap } from '@/features/catalog/sort-map-util';
 import { useValidatedSWR } from '@/shared/hooks/custom-use-swr';
-import {
-  ProductCard,
-  ProductCardsSchema,
-} from '@/shared/schemas/product-card-schema';
-
-export function mapProductToCard(product: ProductProjection): ProductCard {
-  const variant = product.masterVariant;
-  const priceInfo = variant.prices?.[0];
-
-  const attributes = Object.fromEntries(
-    (variant.attributes ?? []).map((attr) => [attr.name, attr.value]),
-  );
-
-  const ratingAttribute: unknown = attributes.rating;
-  const rating =
-    typeof ratingAttribute === 'string'
-      ? parseFloat(String(ratingAttribute))
-      : undefined;
-
-  const country =
-    typeof attributes.country === 'string' ? attributes.country : '';
-  const year = typeof attributes.year === 'string' ? attributes.year : '';
-
-  const description =
-    product.description && typeof product.description['en-US'] === 'string'
-      ? product.description['en-US']
-      : undefined;
-
-  return {
-    id: product.id,
-    key: product.key ?? '',
-    name: product.name['en-US'],
-    image: variant.images?.[0]?.url ?? '',
-    price: priceInfo?.value.centAmount ? priceInfo.value.centAmount / 100 : 0,
-    discountedPrice: priceInfo?.discounted?.value.centAmount
-      ? priceInfo.discounted.value.centAmount / 100
-      : undefined,
-    currency: priceInfo?.value.currencyCode ?? 'EUR',
-    rating:
-      rating && !isNaN(rating) ? Math.min(Math.max(rating, 0), 5) : undefined,
-    country,
-    year,
-    description,
-  };
-}
-
-const sortMap: Record<string, string> = {
-  price_asc: 'price asc',
-  price_desc: 'price desc',
-  rating_desc: 'variants.attributes.rating desc',
-  rating_asc: 'variants.attributes.rating asc',
-  year_desc: 'variants.attributes.year desc',
-  year_asc: 'variants.attributes.year asc',
-};
+import { ProductCardsSchema } from '@/shared/schemas/product-card-schema';
 
 export function useProductCards({
   categoryIds = [],
