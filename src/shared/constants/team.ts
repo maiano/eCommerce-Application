d7ea6776146031cb34afd56131e5bdbecf0c7c0e
ann-sm <annsmirnovva@gmail.com>
2025-06-14 18:04:34 +0300

    chore: update team member descriptions

--- src/shared/constants/team.ts
+++ src/shared/constants/team.ts
@@ -11,7 +11,7 @@ export const team = [
     avatar: '../../src/assets/avatar_3.jpeg',
     name: 'Victoria Ternovaya',
     role: 'Wine Educator',
-    description: `I'm passionate about frontend development and have a deep interest in functional programming languages and architecture`,
+    description: `I hold a degree in Philology, with professional experience as a news editor and 3D visualizer. Since 2024, I've been studying frontend development.`,
     contributions: ['Project design','CommerceTools project and API client setup', 'Main Page', 'Catalog Page', 'Product Page', 'Cart Page', '404 Page'],
     github: 'https://github.com/blk-thorn',
   }, 
@@ -19,7 +19,7 @@ export const team = [
     avatar: '../../src/assets/avatar_2.png',
     name: 'Anna Smirnova',
     role: 'Junior Wine Specialist',
-    description: `I'm passionate about frontend development and have a deep interest in functional programming languages and architecture.`,
+    description: `Passionate about the creative, versatile and dynamic nature of frontend development. Always learning and excited for new challenges.`,
     contributions: ['Task board set up and management', 'Registration Page', 'User Profile Page', 'About Us Page'],
     github: 'https://github.com/ann-sm', 
   }
