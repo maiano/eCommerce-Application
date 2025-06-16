d605f139d0abb552539cfe70cb07478ea5509b38
maiano <maiano@yandex.ru>
2025-06-16 21:27:36 +0200

    fix: fix assets

--- src/shared/constants/team.ts
+++ src/shared/constants/team.ts
@@ -1,26 +1,51 @@
+import avatar1 from '@/assets/avatar_1.png';
+import avatar2 from '@/assets/avatar_2.png';
+import avatar3 from '@/assets/avatar_3.jpeg';
+
 export const team = [
   {
-    avatar: '../../src/assets/avatar_1.png',
+    avatar: avatar1,
     name: 'Aleksei Kalimullin',
     role: 'Lead Sommelier',
     description: `I'm passionate about frontend development and have a deep interest in functional programming languages and architecture.`,
-    contributions: ['Repository set up and project configuration', 'Routing implementaion', 'API client setup', 'Login Page', 'Catalog Page', 'Product Page', 'Writing tests for the entire project'],
-    github: 'https://github.com/maiano', 
+    contributions: [
+      'Repository set up and project configuration',
+      'Routing implementaion',
+      'API client setup',
+      'Login Page',
+      'Catalog Page',
+      'Product Page',
+      'Writing tests for the entire project',
+    ],
+    github: 'https://github.com/maiano',
   },
   {
-    avatar: '../../src/assets/avatar_3.jpeg',
+    avatar: avatar3,
     name: 'Victoria Ternovaya',
     role: 'Wine Educator',
     description: `I hold a degree in Philology, with professional experience as a news editor and 3D visualizer. Since 2024, I've been studying frontend development.`,
-    contributions: ['Project design','CommerceTools project and API client setup', 'Main Page', 'Catalog Page', 'Product Page', 'Cart Page', '404 Page'],
+    contributions: [
+      'Project design',
+      'CommerceTools project and API client setup',
+      'Main Page',
+      'Catalog Page',
+      'Product Page',
+      'Cart Page',
+      '404 Page',
+    ],
     github: 'https://github.com/blk-thorn',
-  }, 
+  },
   {
-    avatar: '../../src/assets/avatar_2.png',
+    avatar: avatar2,
     name: 'Anna Smirnova',
     role: 'Junior Wine Specialist',
     description: `Passionate about the creative, versatile and dynamic nature of frontend development. Always learning and excited for new challenges.`,
-    contributions: ['Task board set up and management', 'Registration Page', 'User Profile Page', 'About Us Page'],
-    github: 'https://github.com/ann-sm', 
-  }
-]
+    contributions: [
+      'Task board set up and management',
+      'Registration Page',
+      'User Profile Page',
+      'About Us Page',
+    ],
+    github: 'https://github.com/ann-sm',
+  },
+];
